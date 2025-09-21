9bae34b7fd6bf4f68925b9dbb7be2503bd7a0bb1
TNTSuperMan <tnt-super-man@outlook.com>
2025-09-21 22:48:59 +0900

    Dateを実装

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,16 @@
 export type Serialized =
     string | number | boolean | null
-    | [8] | [9] | [10] // NaN, Infinity, -Infinity
+    | [9] | [10] | [11] // NaN, Infinity, -Infinity
     | [] // undefined
     | [string] // bigint
     | [0, number, string | boolean | number] // Wrapper
-    | [1, number, string, string] // RegExp
-    | [2, number, string, string?] // Blob
-    | [3, number, string, string, (string|null)?, number?] // File(hex,name,type,last)
-    | [4, number, number, string] // ArrayBuffer/TypedArray/DataView(type,hex)
-    | [5, number, ...Serialized[]] // Array
+    | [1, number, number] // Date
+    | [2, number, string, string] // RegExp
+    | [3, number, string, string?] // Blob
+    | [4, number, string, string, (string|null)?, number?] // File(hex,name,type,last)
+    | [5, number, number, string] // ArrayBuffer/TypedArray/DataView(type,hex)
+    | [6, number, ...Serialized[]] // Array
     | [{ [key: string]: Serialized }, number] // Object
-    | [6, number, [Serialized, Serialized][]] // Map
-    | [7, number, ...Serialized[]] // Set
+    | [7, number, [Serialized, Serialized][]] // Map
+    | [8, number, ...Serialized[]] // Set
     | [null, number] // Reference(11<n)
